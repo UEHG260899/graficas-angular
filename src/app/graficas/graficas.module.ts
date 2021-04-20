f65248e7650316fd25f4f10a559592a1a48f7826
Uriel <urielenrique99@gmail.com>
2021-04-20 17:39:29 -0500

    Estructura basica creada

---
+++ src/app/graficas/graficas.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraficasRoutingModule } from './graficas-routing.module';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { GraficasDoblesComponent } from './pages/graficas-dobles/graficas-dobles.component';
+import { DonaComponent } from './pages/dona/dona.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
+import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+
+
+@NgModule({
+  declarations: [
+    BarrasComponent,
+    GraficasDoblesComponent,
+    DonaComponent,
+    DonaHttpComponent,
+    GraficaBarraComponent
+  ],
+  imports: [
+    CommonModule,
+    GraficasRoutingModule
+  ]
+})
+export class GraficasModule { }
---
+++ src/app/graficas/pages/graficas-dobles/graficas-dobles.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas-dobles',
+  templateUrl: './graficas-dobles.component.html',
+  styles: [
+  ]
+})
+export class GraficasDoblesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
