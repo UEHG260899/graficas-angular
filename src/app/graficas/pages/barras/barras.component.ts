e3a9c4c3eed583c3b6790a9af6b814e827ef8068
Uriel <urielenrique99@gmail.com>
2021-04-20 18:32:21 -0500

    FUncionalidad de la grafica de barras completa

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-barras',
@@ -8,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarrasComponent implements OnInit {
 
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
+  };
+  public barChartLabels: Label[] = ['2020', '2021', '2022', '2023', '2024', '2026', '2026'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+
+
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#FFEBA1', hoverBackgroundColor: '#FFEBA1'},
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#E8AE8E', hoverBackgroundColor: '#E8AE8E'},
+    { data: [8, 18, 90, 59, 26, 37, 56], label: 'Series C' , backgroundColor: '#FFA8D8', hoverBackgroundColor: '#FFA8D8'},
+  ];
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  public randomize(): void {
+    this.barChartData[0].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ]
+
+    this.barChartData[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ]
+
+    this.barChartData[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ]
   }
 
 }
